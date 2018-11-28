cde9dbd73adf6d8c0225792cf8eb15ba6695558c
Thomas Martin <tmartin.dev101010@gmail.com>
2018-11-28 13:05:26 -0700

    Database files created and tested.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("All American Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mini Burger", false);
