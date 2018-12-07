45c3dcec14065d85f1cf2b9b3952cedf5af8b6dd
Thomas Martin <tmartin.dev101010@gmail.com>
2018-12-07 13:46:15 -0700

    API routes are working correctly.

---
+++ models/burger.js
@@ -0,0 +1,26 @@
+var orm = require("../config/orm");
+
+// selectAll()
+// insertOne()
+// updateOne()
+var burger = {
+  selectAll: function (cb) {
+    orm.selectAll("burgers", function (result) {
+      cb(result);
+    });
+  },
+
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(result) {
+      cb(result);
+    });
+  },
+
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(result) {
+      cb(result);
+    });
+  }
+};
+
+module.exports = burger;
